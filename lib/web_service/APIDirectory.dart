320f642f39c14308b638f82f2142c40bf93d4b65
Sumit Trivedi <sumittrivedi073@gmail.com>
2024-12-30 17:01:04 +0530

    initial commite

--- lib/web_service/APIDirectory.dart
+++ lib/web_service/APIDirectory.dart
@@ -8,7 +8,7 @@ const mobileHost = '192.168.29.211';
 const webBaseUrl = '$scheme://$host:$port';
 const mobileBaseUrl = '$scheme://$mobileHost:$port';
 
-const productionUrl = 'https://h2gvbfqo6smwzaby6x7jinzujm0otrjt.lambda-url.eu-west-2.on.aws/';
+const productionUrl = 'https://prcddzpsk5p33eazh5ykzbj4ra0xwyhk.lambda-url.eu-west-2.on.aws/';
 const directionBaseURL = 'https://maps.googleapis.com/maps/api/directions/json';
 
 
